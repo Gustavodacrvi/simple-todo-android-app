0200b792dd3f39464d0ecee965deaa06497ed272
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-04-13 13:10:12 -0300

    deleted user_input file and added layout directly on content_main.xml

--- app/src/main/java/com/example/simpletodoapp/MainActivity.java
+++ app/src/main/java/com/example/simpletodoapp/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.simpletodoapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-  String[] todos = {};
+  String[] todos = {"please", "kill", "me"};
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 	setContentView(R.layout.activity_main);
 	Toolbar toolbar = findViewById(R.id.toolbar);
 	setSupportActionBar(toolbar);
+
+	createListView();
 
 	FloatingActionButton fab = findViewById(R.id.fab);
 	fab.setOnClickListener(new View.OnClickListener() {
@@ -30,8 +38,6 @@ public class MainActivity extends AppCompatActivity {
 				.setAction("Action", null).show();
 	  }
 	});
-
-	createListView();
   }
 
   private void createListView() {
